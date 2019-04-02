54c1985049872959691304e1d6fd766bf26562d9
AFei <super_afei@qq.com>
2019-04-02 18:20:04 +0800

    完成MailBean

---
+++ MailServer/src/main/java/com/afei/beans/MailBean.java
@@ -0,0 +1,87 @@
+package com.afei.beans;
+
+import java.util.Date;
+
+public class MailBean {
+
+    private String from; // 发件人
+    private String to; // 收件人
+    private String subject; // 主题
+    private String content; // 内容
+    private Date date; // 发件时间
+
+    public MailBean() {
+        date = new Date();
+    }
+
+    /**
+     * @return the from
+     */
+    public String getFrom() {
+        return from;
+    }
+
+    /**
+     * @param from the from to set
+     */
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    /**
+     * @return the to
+     */
+    public String getTo() {
+        return to;
+    }
+
+    /**
+     * @param to the to to set
+     */
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    /**
+     * @return the subject
+     */
+    public String getSubject() {
+        return subject;
+    }
+
+    /**
+     * @param subject the subject to set
+     */
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    /**
+     * @return the content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * @param content the content to set
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    /**
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+}
