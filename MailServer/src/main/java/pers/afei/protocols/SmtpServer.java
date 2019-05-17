b09dc8e80ea148273d6b3fe47a5d32ff843052f2
AFei <super_afei@qq.com>
2019-05-17 23:21:22 +0800

    完成SMTP协议，放弃POP3协议，反正写法差不多，就懒地写了~~~

--- MailServer/src/main/java/pers/afei/protocols/SmtpServer.java
+++ MailServer/src/main/java/pers/afei/protocols/SmtpServer.java
@@ -9,10 +9,12 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Vector;
 
+import pers.afei.utils.Database;
 import pers.afei.utils.Utils;
 
 import org.apache.commons.codec.binary.Base64;
@@ -23,6 +25,7 @@ public class SmtpServer implements Server {
 
     private static final Map<String, String> mp;
     private static final Map<String, Boolean> code;
+
     static {
         /*
          * 服务器响应
@@ -30,41 +33,40 @@ public class SmtpServer implements Server {
         mp = new HashMap<String, String>();
 
         // 命令输入错误
-        // mp.put("NoCOMMAND", "502 Error: auth command not implemented");
+        mp.put("NoCOMMAND", "502 Error: auth command not implemented\r\n");
 
         // 通用邮箱操作指令
-        // mp.put("OK", "250 OK");
+        mp.put("OK", "250 OK\r\n");
 
         // 正常流程
-        // mp.put("SERVEROK", "220 smtp.afei.com Smtp AFei Mail Server");
-        // mp.put("HELO", "250 stmp.afei.com");
-        // mp.put("USER", "334 " + Base64.encodeBase64String("User:".getBytes()));
-        // mp.put("PASSWORD", "334 " +
-        // Base64.encodeBase64String("Password:".getBytes()));
-        // mp.put("AUTHSucc", "235 Authentication successful");
-        // mp.put("DATAStart", "354 End data with <CR><LF>.<CR><LF>");
-        // mp.put("DATAEnd", "250 Ok: queued as");
-        // mp.put("QUIT", "221 Bye");
+        mp.put("SERVEROK", "220 smtp.afei.com Smtp AFei Mail Server\r\n");
+        mp.put("HELO", "250 stmp.afei.com\r\n");
+        mp.put("USER", "334 " + Base64.encodeBase64String("User:".getBytes()) + "\r\n");
+        mp.put("PASSWORD", "334 " + Base64.encodeBase64String("Password:".getBytes()) + "\r\n");
+        mp.put("AUTHSucc", "235 Authentication successful\r\n");
+        mp.put("DATAStart", "354 End data with <CR><LF>.<CR><LF>\r\n");
+        mp.put("DATAEnd", "250 Ok: queued as\r\n");
+        mp.put("QUIT", "221 Bye\r\n");
 
         // 流程顺序错误
-        // mp.put("NoHELO", "503 Error: send HELO first");
-        // mp.put("NoSMTP", "503 Error: need HELO and AUTH first !");
-        // mp.put("NoMAIL", "503 Error: need MAIL command");
-        // mp.put("NoRCPT", "503 Error: need RCPT command");
+        mp.put("NoHELO", "503 Error: send HELO first\r\n");
+        mp.put("NoSMTP", "503 Error: need HELO and AUTH first !\r\n");
+        mp.put("NoMAIL", "503 Error: need MAIL command\r\n");
+        mp.put("NoRCPT", "503 Error: need RCPT command\r\n");
 
         // 语法错误
-        // mp.put("HELOSyntax", "501 Syntax: HELO hostname");
-        // mp.put("AUTHSyntax", "502 Error: auth command not implemented");
-        mp.put("MAILSyntax", "501 Syntax: MAIL FROM: <address>");
-        mp.put("RCPTSyntax", "501 Syntax: RCPT TO: <address>");
+        mp.put("HELOSyntax", "501 Syntax: HELO hostname\r\n");
+        mp.put("AUTHSyntax", "502 Error: auth command not implemented\r\n");
+        mp.put("MAILSyntax", "501 Syntax: MAIL FROM: <address>\r\n");
+        mp.put("RCPTSyntax", "501 Syntax: RCPT TO: <address>\r\n");
 
         // 其它问题
-        // mp.put("AUTHFail", "535 Error: authentication failed");
-        mp.put("MAILFromError", "501 mail from address must be same as authorization user");
-        // mp.put("MAILFormatError", "501 Error: malformed authentication response");
-        // mp.put("MAILAddrError", "500 Error: Incorrect email address");
-        mp.put("MAILBoxNotFound", "550 Mailbox not found or access denied");
-        mp.put("MAILStorageOverrun", "452 Error: Insufficient storage");
+        mp.put("AUTHFail", "535 Error: authentication failed\r\n");
+        mp.put("MAILFromError", "501 mail from address must be same as authorization user\r\n");
+        mp.put("MAILFormatError", "501 Error: malformed authentication response\r\n");
+        mp.put("MAILAddrError", "500 Error: Incorrect email address\r\n");
+        mp.put("MAILBoxNotFound", "550 Mailbox not found or access denied\r\n");
+        mp.put("MAILStorageOverrun", "452 Error: Insufficient storage\r\n");
 
         /*
          * SMTP命令
@@ -78,6 +80,7 @@ public class SmtpServer implements Server {
         code.put("rcpt", true);
         code.put("data", true);
         code.put("quit", true);
+
     }
 
     public SmtpServer() throws IOException {
@@ -102,28 +105,24 @@ public class SmtpServer implements Server {
         String user = null; // 存发信人
         Vector<String> rcpts = new Vector<String>(); // 存收信人
         int sizeFrom = 0; // 发信人剩余空间
-        Vector<Integer> sizeTo = new Vector<Integer>(); // 存收信人剩余空间
 
         while ((str = bufferIn.readLine()) != null) {
-            // 读入一行
+
+            // 删除首尾空白
+            str = str.trim();
+
+            // 单个字符或者空字符串直接跳过
+            if (str.length() <= 1) {
+                continue;
+            }
+
+            // 直接将输入转成小写字符
             String lowerCase = str.toLowerCase();
 
+            // 分割输入命令
             String[] arrayStrings = lowerCase.split(" ");
 
-            if (arrayStrings[0].equals("data")) {
-                // 收到data命令时
-                if (rcpts.size() == 0) {
-                    out.write(mp.get("NoRCPT").getBytes());
-
-                } else {
-                    out.write(mp.get("DATAStart").getBytes());
-                    for (String rcpt : rcpts) {
-                        /* 发邮件主体~~ */
-                    }
-
-                    out.write(mp.get("DATAEnd").getBytes());
-                }
-            } else if (code.get(arrayStrings[0]) != true) {
+            if (code.get(arrayStrings[0]) == null || code.get(arrayStrings[0]) != true) {
                 // 无此命令
                 out.write(mp.get("NoCOMMAND").getBytes());
 
@@ -173,7 +172,7 @@ public class SmtpServer implements Server {
                                 out.write(mp.get("MAILFormatError").getBytes());
 
                             } else {
-                                user = Base64.decodeBase64(str).toString();
+                                user = new String(Base64.decodeBase64(str));
 
                                 /** 判邮箱是否合法 **/
                                 String[] tmp = user.split("@");
@@ -191,15 +190,12 @@ public class SmtpServer implements Server {
                                             out.write(mp.get("MAILFormatError").getBytes());
 
                                         } else {
-                                            String pass = Base64.decodeBase64(str).toString();
+                                            String pass = new String(Base64.decodeBase64(str));
                                             String sql = "SELECT user_mailstorage FROM USER WHERE "
-                                                    + "USER_NAME = ?, USER_PASSWORD = ?;";
-                                            Vector<String> v = new Vector<String>();
-                                            v.add(user);
-                                            v.add(pass);
+                                                    + "USER_NAME = ? AND USER_PASSWORD = ?;";
 
-                                            ResultSet rs = Server.db.executeQuery(sql, v);
-                                                
+                                            ResultSet rs = Server.db.executeQuery(sql, user, pass);
+
                                             try {
                                                 rs.next();
                                                 auth = true;
@@ -217,14 +213,154 @@ public class SmtpServer implements Server {
                         }
                     }
 
-                } else if(arrayStrings[0].equals("rcpt")) {
+                } else if (arrayStrings[0].equals("rcpt")) {
                     out.write(mp.get("NoMAIL").getBytes());
-                
+
                 } else {
                     out.write(mp.get("NoSMTP").getBytes());
                 }
-            } else {
+            } else if (arrayStrings[0].equals("mail")) {
+
+                char[] codeString = lowerCase.substring(arrayStrings[0].length()).trim().toCharArray();
+
+                boolean flag = codeString.length > 4;
+
+                flag = flag && codeString[0] == 'f' && codeString[1] == 'r' && codeString[2] == 'o'
+                        && codeString[3] == 'm';
+
+                flag = flag && (lowerCase.indexOf("<") * lowerCase.indexOf(">") > 0);
+
+                int i = 4;
+                while (i < codeString.length) {
+                    if (codeString[i] == ' ') {
+                        ++i;
+                    } else {
+                        break;
+                    }
+                }
+                flag = flag && codeString[i] == ':';
+
+                if (!flag) {
+                    out.write(mp.get("MAILSyntax").getBytes());
+                } else {
+                    String sender = lowerCase.indexOf("<") == -1 ? lowerCase.substring(lowerCase.indexOf(":") + 1)
+                            : lowerCase.substring(lowerCase.indexOf("<") + 1, lowerCase.indexOf(">"));
+
+                    if (!sender.trim().equals(user)) {
+                        out.write(mp.get("MAILFromError").getBytes());
+                    } else {
+                        out.write(mp.get("OK").getBytes());
+                    }
+                }
+
+            } else if (arrayStrings[0].equals("rcpt")) {
+
+                char[] codeString = lowerCase.substring(arrayStrings[0].length()).trim().toCharArray();
+
+                boolean flag = codeString.length > 2;
+
+                flag = flag && codeString[0] == 't' && codeString[1] == 'o';
+                flag = flag && (lowerCase.indexOf("<") * lowerCase.indexOf(">") > 0);
+
+                int i = 2;
+                while (i < codeString.length) {
+                    if (codeString[i] == ' ') {
+                        ++i;
+                    } else {
+                        break;
+                    }
+                }
+                flag = flag && codeString[i] == ':';
+
+                if (!flag) {
+                    out.write(mp.get("RCPTSyntax").getBytes());
+                } else {
+                    String rcpt = lowerCase.indexOf("<") == -1 ? lowerCase.substring(lowerCase.indexOf(":") + 1)
+                            : lowerCase.substring(lowerCase.indexOf("<") + 1, lowerCase.indexOf(">"));
+
+                    rcpts.add(rcpt);
+                    out.write(mp.get("OK").getBytes());
+                }
+
+            } else if (arrayStrings[0].equals("data")) {
+                // 收到data命令时
+                if (rcpts.size() == 0) {
+                    out.write(mp.get("NoRCPT").getBytes());
+
+                } else {
+                    out.write(mp.get("DATAStart").getBytes());
+
+                    StringBuilder mail = new StringBuilder();
+
+                    boolean dataStartB = false;
+                    boolean subjectB = false, fromB = false, toB = false, dateB = false;
+                    String subjectString = null; 
+
+                    while (!(str = bufferIn.readLine()).equals(".")) {
+
+                        mail.append(str + "\r\n");
+                        if (!dataStartB) {
+                            
+                            int t = str.indexOf(":");
+
+                            if (t == -1) {
+                                dataStartB = true;
+                            } else {
+                                String head = str.substring(0, t).toLowerCase().trim();
+
+                                if (head.equals("subject") && !subjectB) {
+                                    subjectB = true;
+                                    subjectString = str.substring(t + 1).trim();
+                                } else if (head.equals("from") && !fromB) {
+                                    fromB = true;
+                                } else if (head.equals("to") && !toB) {
+                                    toB = true;
+                                } else if (head.equals("date") && !dateB) {
+                                    dateB = true;
+                                } else {
+                                    dataStartB = true;
+                                }
+                            }
+                        }
+                    }
+
+                    String sql = "INSERT INTO MAIL(MAIL_FROM, MAIL_TO, MAIL_SUBJECT, MAIL_CONTENT, MAIL_DATE) VALUES (?, ?, ?, ?, ?);";
+                    String con = new String(mail);
+                    int length = con.getBytes().length;
+
+                    if(length <= sizeFrom) {
+                        
+                        Server.db.execute("UPDATE USER SET USER_MAILSTORAGE=? WHERE USER_NAME=?", String.valueOf(sizeFrom-length), user);
+                        for (String rcpt : rcpts) {
+
+                            ResultSet rs = Server.db.executeQuery("SELECT user_mailstorage FROM USER WHERE USER_NAME = ?", rcpt);
+                            int sizeTo = 0;
+                            try {
+                                rs.next();
+                                out.write(mp.get("AUTHSucc").getBytes());
+                                sizeTo = Integer.valueOf(rs.getString(1));
+
+                            } catch (SQLException e) {
+                                
+                            }
+
+                            /* 发邮件主体~~ */
+                            if(sizeTo >= length) {
+                                Server.db.execute(sql, user, rcpt, subjectString, new String(mail), Database.utilDateToSqlDate(new Date()));
+                                Server.db.execute("UPDATE USER SET USER_MAILSTORAGE=? WHERE USER_NAME=?", String.valueOf(sizeTo-length), rcpt);
+                            } else {
+                                out.write(mp.get("MAILStorageOverrun").getBytes());
+                            }
                 
+                        }
+                    } else {
+                        out.write(mp.get("MAILStorageOverrun").getBytes());
+                    }
+
+                    rcpts.clear();
+
+                    out.write(mp.get("DATAEnd").getBytes());
+                }
             }
         }
     }
@@ -242,6 +378,5 @@ public class SmtpServer implements Server {
         SmtpServer server = new SmtpServer(25);
         server.run();
     }
-
 
 }
